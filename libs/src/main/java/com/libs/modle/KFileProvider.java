4c68e405a389923c54bd7a8223a41722fc53fe35
419360214@qq.com <130681li>
2020-09-12 10:11:09 +0800

    解决provder冲突

---
+++ libs/src/main/java/com/libs/modle/KFileProvider.java
@@ -0,0 +1,11 @@
+package com.libs.modle;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * @ author：mo
+ * @ data：2020/9/11:17:54
+ * @ 功能：
+ */
+public class KFileProvider extends FileProvider {
+}
